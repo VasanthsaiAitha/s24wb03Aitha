6d9d118c3044f0666db935e60dab1c19acd95647
VasanthsaiAitha <aithavasanthsai@gmail.com>
2024-03-22 07:39:07 -0500

    created randomitem.pug and pick.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var MusicalInstrumentsRouter = require('./routes/MusicalInstruments')
 var gridrsRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick')
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/MusicalInstruments',MusicalInstrumentsRouter);
 app.use('/grid',gridrsRouter);
+app.use('/pick',pickRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
